b1e2d7127ccc9346f0a104b010f9016a108c1278
Jose Bautista <Cooldj1020@gmail.com>
2021-07-01 13:05:53 -0600

    adding theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import {ChakraProvider } from "@chakra-ui/react"
+import { Meta } from "../lib/meta"
+import { customTheme } from '../lib/theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme = {customTheme}>
+      <Meta/>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
