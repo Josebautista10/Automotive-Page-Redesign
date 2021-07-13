ae9757597bf60e16df661230c1f8c59066017136
Jose Bautista <Cooldj1020@gmail.com>
2021-07-13 07:36:36 +0100

    Adding footer

---
+++ components/carousel-image/index.js
@@ -0,0 +1,11 @@
+import { Box, Image, Flex, Text } from '@chakra-ui/react';
+
+export const CarouselImage = () => {
+  return (
+    <>
+      <Box boxSize="sm">
+        <Image src="/about-us/white-skyline.jpeg" alt="white-skyline" />
+      </Box>
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
 import { Header } from '../components/header';
-import { Box, Heading, Text, SimpleGrid, Flex } from '@chakra-ui/react';
 import { Service } from '../components/service';
+import { CarouselImage } from '../components/carousel-image';
+import { Toast } from '../components/toast';
+import {
+  Box,
+  Heading,
+  Text,
+  SimpleGrid,
+  Flex,
+  Center,
+  HStack,
+  Square,
+  FormLabel,
+  Input,
+  FormHelperText,
+  FormControl,
+  Button,
+} from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <>
-      <Box
-        width="100vw"
-        background="brand.100"
-        color="brand.300"
-      >
+      <Box width="100vw" background="brand.100" color="brand.300">
         <Header></Header>
         <Box
           backgroundImage="/images/afr-image.webp"
@@ -42,35 +54,80 @@ export default function Home() {
           <Service name="Service" img="/images/oil-afr.webp" />
           <Service name="Electrical" img="/images/electrical.jpeg" />
         </SimpleGrid>
-        <Box bgGradient="linear(to-b, #53D8EF, #000000)">
-          <Text
-            bgClip="text"
-            bgGradient="linear(to-br, #000000, #FFFFFF)"
-            fontSize="48px"
-            marginLeft="400px"
-            marginBottom="10px"
-            fontWeight="bolder"
-          >
-            ABOUT US
-          </Text>
-          <Flex padding="50px">
-            <Box maxWidth="50%">
-              <Text fontSize="25px" fontWeight="semibold" marginBottom="20px">
-                Looking for a local shop you can entrust your vehicle to? We
-                started AFR Autoworks in order to provide the best service at a
-                fair price. Our technicians are knowledgable, we are well
-                equipped, and can tackle the simplest repair to the most
-                technical of challenges.
-              </Text>
-              <Text fontSize="25px" fontWeight="semibold">
-                We Service all makes and models, modified or stock, wether
-                you're after a simple oil change and inspection or you're after
-                some Performance upgrades and Dyno tuning we have you covered!
-              </Text>
-            </Box>
-            <Box>images</Box>
-          </Flex>
-        </Box>
+        <HStack bgGradient="linear(to-b, #53D8EF, #000000)">
+          <Box maxWidth="50%">
+            <Text
+              align="center"
+              bgClip="text"
+              bgGradient="linear(to-br, #000000, #FFFFFF)"
+              fontSize="48px"
+              // marginLeft="400px"
+              // marginBottom="10px"
+              fontWeight="bolder"
+            >
+              ABOUT US
+            </Text>
+            <Text
+              fontSize="25px"
+              fontWeight="semibold"
+              marginBottom="20px"
+              align="center"
+            >
+              Looking for a local shop you can entrust your vehicle to? We
+              started AFR Autoworks in order to provide the best service at a
+              fair price. Our technicians are knowledgable, we are well
+              equipped, and can tackle the simplest repair to the most technical
+              of challenges.
+            </Text>
+            <Text fontSize="25px" fontWeight="semibold" align="center">
+              We Service all makes and models, modified or stock, wether you're
+              after a simple oil change and inspection or you're after some
+              Performance upgrades and Dyno tuning we have you covered!
+            </Text>
+          </Box>
+
+          <CarouselImage></CarouselImage>
+        </HStack>
+        <HStack bgGradient="linear(to-b, #53D8EF, #000000)">
+          <Box maxWidth="49%">
+            <Text
+              align="center"
+              bgClip="text"
+              bgGradient="linear(to-br, #000000, #FFFFFF)"
+              fontSize="48px"
+              fontWeight="bolder"
+            >
+              AFR Autoworks
+            </Text>
+            <Text
+              fontSize="25px"
+              fontWeight="semibold"
+              marginBottom="20px"
+              align="center"
+            >
+              Love Your Car. Do What Others Don't to get Results That Others
+              Won't.
+            </Text>
+          </Box>
+          <Box maxWidth="49%">
+            <Text
+              align="center"
+              bgClip="text"
+              bgGradient="linear(to-br, #000000, #FFFFFF)"
+              fontSize="48px"
+              fontWeight="bolder"
+            >
+              Never Miss a Newsletter
+            </Text>
+            <FormControl id="email" is="required">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+              <Toast></Toast>
+
+              <FormHelperText>We'll never share your email.</FormHelperText>
+            </FormControl>
+          </Box>
+        </HStack>
       </Box>
     </>
   );
